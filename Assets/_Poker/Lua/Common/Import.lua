31f6eb7e08c2a95beaf917299f2f60618dc726d1
Canyon <379657505@qq.com>
2017-04-20 09:22:59 +0800

    format

--- Assets/_Poker/Lua/Common/Import.lua
+++ Assets/_Poker/Lua/Common/Import.lua
@@ -1,117 +1,121 @@
 --- 加载工具
 -- Anchor : Canyon
 -- Time : 2017-04-18 11:25
--- Desc : 
+-- Desc :
 -- Modify :
 
 do
 
--- 全局变量
-local MG = _G;
+  -- 全局变量
+  local MG = _G;
 
-local M = {};
+  local M = {};
 
-local that = M;
+  local that = M;
 
-local package = package;
+  local package = package;
 
-local string = string;
+  local string = string;
 
--- 记录加载了的Lua脚本
-local recordRequire = {};
+  -- 记录加载了的Lua脚本
+  local recordRequire = {};
 
--- 全局
-function M.Consts(tab)
-  local tp = type(tab);
-  
-  if tp ~= "table" then
-    return;
-  end
-  
-  for key, var in pairs(tab) do
-  	that.ConstsKV(key,var);
-  end
-end
+  -- 全局
+  function M.Consts(tab)
+    local tp = type(tab);
 
--- 全局
-function M.ConstsKV(k,v)
-  local tp = type(k);
-  
-  if tp ~= "string" then
-    return;
-  end
-  MG[k] = v;
-end
-
--- 移除全局
-function M.RemoveOne(k)
-	MG[k] = nil;
-end
-
-function M.Require(pars,wrap)
-  local tp = type(pars);
-  
-  if tp == "table" then
-    for _, var in pairs(pars) do
-      that.RequireOne(var,wrap);
+    if tp ~= "table" then
+      return;
     end
-  elseif tp == "string" then
-    that.RequireOne(pars,wrap);
-  end
-end
 
-function M.RequireOne(modname,wrap)
-
-  string.gsub(modname,"/",".");
-
-  if type(wrap) == "string" then
-    modname = wrap .. "." .. modname;
-  end
-  
-  if recordRequire[modname] then
-    return recordRequire[modname];
-  end
-  
-  local function funcMain()
-    return require(modname);	
-  end
-  
-  local funcError = function(msg)
-    if not string.find(msg, string.format("'%s' not found:", modname)) then
-        print("load lua error: " .. msg)
+    for key, var in pairs(tab) do
+      that.ConstsKV(key,var);
     end
   end
-  
-  local status, data = xpcall(funcMain,funcError);
-  
-  local tp = type(data)
-  
-  if status and (tp == "table" or tp == "userdata") then
+
+  -- 全局
+  function M.ConstsKV(k,v)
+    local tp = type(k);
+
+    if tp ~= "string" then
+      return;
+    end
+    MG[k] = v;
+  end
+
+  -- 移除全局
+  function M.RemoveOne(k)
+    MG[k] = nil;
+  end
+
+  function M.Require(pars,wrap)
+    local tp = type(pars);
+
+    if tp == "table" then
+      for _, var in pairs(pars) do
+        that.RequireOne(var,wrap);
+      end
+    elseif tp == "string" then
+      that.RequireOne(pars,wrap);
+    end
+  end
+
+  function M.RequireOne(modname,wrap)
+
+    string.gsub(modname,"/",".");
+
+    if type(wrap) == "string" then
+      string.gsub(wrap,"/",".");
+      modname = wrap .. "." .. modname;
+    end
+
+    if recordRequire[modname] then
+      return recordRequire[modname];
+    end
+
+    local function funcMain()
+      return require(modname);
+    end
+
+    local funcError = function(msg)
+      local msgTip = string.format("'%s' not found:", modname);
+      if string.find(msg, msgTip) then
+        print(msgTip);
+      else
+        print("load lua error: " .. msg);
+      end
+    end
+
+    local status, data = xpcall(funcMain,funcError);
+
+    local tp = type(data)
+
+    if status and (tp == "table" or tp == "userdata") then
       recordRequire[modname] = data;
       return data;
-  end
-end
-
-function M.ClearRequire(pars)
-  local tp = type(pars);
-  
-  if tp == "table" then
-    for _, var in pairs(pars) do
-      that.ClearRequireOne(var);
     end
-  elseif tp == "string" then
-    that.ClearRequireOne(pars);
   end
-end
 
-function M.ClearRequireOne(modname)
-	package.preload[modname] = nil;
-  package.loaded[modname] = nil;
-end
+  function M.ClearRequire(pars)
+    local tp = type(pars);
 
-function M.ClearRequire()
-	that.ClearRequire(recordRequire);
-end
+    if tp == "table" then
+      for _, var in pairs(pars) do
+        that.ClearRequireOne(var);
+      end
+    elseif tp == "string" then
+      that.ClearRequireOne(pars);
+    end
+  end
 
-return M;
+  function M.ClearRequireOne(modname)
+    package.preload[modname] = nil;
+    package.loaded[modname] = nil;
+  end
+
+  function M.ClearRequire()
+    that.ClearRequire(recordRequire);
+  end
+
+  return M;
 end
