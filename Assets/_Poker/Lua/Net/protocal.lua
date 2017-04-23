f1d600d7fbbd1c5b606da7278de1a8feda004355
Canyon <379657505@qq.com>
2017-04-23 19:05:52 +0800

    移动位置

--- Assets/_Poker/Lua/Net/protocal.lua
+++ Assets/_Poker/Lua/Net/protocal.lua
@@ -1,10 +1,11 @@
---Buildin Table
+-- 协议 常量
 Protocal = {
+  Unload   = 'Unload';  -- 通知销毁
+  OnSuccessConnect = '100';  --连接服务器成功回调
 	Connect		= '101';	--连接服务器
 	Exception   = '102';	--异常掉线
 	Disconnect  = '103';	--正常断线   
 	Message		= '104';	--接收消息
-	Msg         = '1111';	--接收消息
 }
 
 --协议类型--
@@ -14,6 +15,3 @@ ProtocalType = {
   PBC = 2,
   SPROTO = 3,
 }
-
---当前使用的协议类型--
-TestProtoType = ProtocalType.PB_LUA;
