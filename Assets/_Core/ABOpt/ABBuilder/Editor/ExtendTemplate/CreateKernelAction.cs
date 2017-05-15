f0873713d529c8b1aeb66bc6c9389b47d72a3805
canyons <gongyanghui1986@163.com>
2017-05-15 21:05:52 +0800

    代码控制自己脚本的样式，自定义创建的脚本样式，还可以修改他里面模版的样式

---
+++ Assets/_Core/ABOpt/ABBuilder/Editor/ExtendTemplate/CreateKernelAction.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.ProjectWindowCallback;
+using System.IO;
+using UnityEditor;
+
+/// <summary>
+/// 类名 : 定义一个创建资源的 Kernel 类并实现其Kernel方法
+/// 作者 : Canyon
+/// 日期 : 2017-05-15 20:45
+/// 功能 : 
+/// </summary>
+public class CreateKernelAction : EndNameEditAction {
+	public override void Action (int instanceId, string pathName, string resourceFile)
+	{
+		//创建资源
+		Object obj = CreateAssetFormTemplate(pathName, resourceFile);
+		//高亮显示该资源
+		ProjectWindowUtil.ShowCreatedAsset(obj);
+	}
+
+	internal static Object CreateAssetFormTemplate(string pathName, string resourceFile)
+	{
+
+		//获取要创建资源的绝对路径
+		string fullName = Path.GetFullPath(pathName);
+		//读取本地模版文件
+		StreamReader reader = new StreamReader(resourceFile);
+		string content = reader.ReadToEnd();
+		reader.Close();
+
+		//获取资源的文件名
+		string fileName = Path.GetFileNameWithoutExtension(pathName);
+
+		//替换默认的文件名
+		content = content.Replace("#SCRIPTNAME#", fileName);
+
+		// 替换时间
+		content = content.Replace("#datatime#",string.Format("{0:yyyy-MM-dd HH:mm}",System.DateTime.Now));
+
+		//写入新文件
+		StreamWriter writer = new StreamWriter(fullName, false, System.Text.Encoding.UTF8);
+		writer.Write(content);
+		writer.Close();
+
+		//刷新本地资源
+		AssetDatabase.ImportAsset(pathName);
+		AssetDatabase.Refresh();
+
+		return AssetDatabase.LoadAssetAtPath(pathName, typeof(Object));
+	}
+}
---
+++ Assets/_Core/ABOpt/ABBuilder/Editor/ExtendTemplate/ScriptTempExtend.cs
@@ -0,0 +1,44 @@
+﻿using UnityEditor;
+using System.Collections;
+using System.IO;
+using UnityEngine;
+
+/// <summary>
+/// 类名 : 扩展创建自己的脚本
+/// 作者 : Canyon
+/// 日期 : 2017-05-15 20:21
+/// 功能 : 
+/// </summary>
+public class ScriptTempExtend :  Editor{
+
+	[MenuItem("Assets/Create/Kernel C# Scripts", false, 91)]
+	static void CreateKernelScripts()
+	{
+		ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0,
+			ScriptableObject.CreateInstance<CreateKernelAction>(),
+			GetSelectedPath() + "/NewKernelScript.cs", null,
+			"Assets/_Core/ABOpt/ABBuilder/Editor/ExtendTemplate/91-C# Kernel Script-NewKernelScript.cs.txt");
+	}
+
+	private static string GetSelectedPath()
+	{
+		//默认路径为Assets
+		string selectedPath = "Assets";
+
+		//获取选中的资源
+		UnityEngine.Object[] selection = Selection.GetFiltered(typeof(Object), SelectionMode.Assets);
+
+		//遍历选中的资源以返回路径
+		foreach (UnityEngine.Object obj in selection)
+		{
+			selectedPath = AssetDatabase.GetAssetPath(obj);
+			if (!string.IsNullOrEmpty(selectedPath) && File.Exists(selectedPath))
+			{
+				selectedPath = Path.GetDirectoryName(selectedPath);
+				break;
+			}
+		}
+
+		return selectedPath;
+	}
+}
