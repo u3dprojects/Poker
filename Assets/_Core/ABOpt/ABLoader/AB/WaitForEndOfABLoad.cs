868fbfcfd19d34b050cdcd597c72ee5b93265302
Canyon <379657505@qq.com>
2017-05-16 18:00:41 +0800

    添加自定义协同程序

---
+++ Assets/_Core/ABOpt/ABLoader/AB/WaitForEndOfABLoad.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 类名 : 自定义资源加载资源协同程序
+/// 作者 : Canyon
+/// 日期 : 2017-05-16 17:53
+/// 功能 : 
+/// </summary>
+public class WaitForEndOfABLoad : CustomYieldInstruction {
+	#region implemented abstract members of CustomYieldInstruction
+	public override bool keepWaiting {
+		get {
+			return false;
+		}
+	}
+	#endregion
+}
